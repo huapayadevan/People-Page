76bbbe7ac627d2261af1bdd81b609ab8e75398e1
Devan's Love of Pugs <huapayadevan@gmail.com>
2013-08-04 17:59:35 -0500

    Modified navigation
    One page scroll is working better, need to refactor to accommodate for
    linkHover function

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $.fn.linkHover = function() {
 // function called when document is loaded
 $(function() {
 
-	//$("a").linkHover();
+	$("li").linkHover();
 
 	$('#nav').onePageNav({
 		currentClass: 'current',
