84ddd63b4c54b468428e2004e9182c9762068466
Devan's Love of Pugs <huapayadevan@gmail.com>
2013-08-10 11:15:25 -0500

    Pageslide complete enough
    Pageslide and other elements satisfactory.
    
    TODO:
    color scheme
    font style
    links for linkedIn and facebook

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ $.fn.linkHover = function() {
 	
 	
 	this.hover(function(){
-		$(this).animate({color: '#800000', textShadow: '2px 2px 4px rgba(255, 255, 255, 0.1)'},150);
+		$(this).animate({color: '#B9E3E3'},200);
 		},function(){
 			if ($(this).hasClass('current')){
 				return;
 			}else{
-				$(this).animate({color: '#FAF6EF'},150);
-				$(this).animate({textShadow:'0px 0px 4px #000'});
+				$(this).animate({color: '#50B9B9'},200  );
 			}
 				
 	});
@@ -19,16 +18,22 @@ $.fn.linkHover = function() {
 // function called when document is loaded
 $(function() {
 	
+        // intiate parallax elements plugin
 	$.stellar({
 		  horizontalScrolling: false
 	});
 	
-	$("li").linkHover();
-
+        // initiate function for animation on course and person hover
+	$(".course").linkHover();
+	$(".person").linkHover();
+        // initiate one page navigation plugin
 	$('#nav').onePageNav({
 		scrollOffset: 60
 	});
+        
+        
 	
+        // hover animation over email contact
 	$(".contact").hover(function(){
 			$(this).animate({color: '#000'},150);
 			$(this).removeClass("blackShadow",150);
@@ -38,5 +43,43 @@ $(function() {
 			$(this).removeClass("whiteShadow", 150);
 			$(this).addClass("blackShadow", 150);
 	});
-	
+        
+        // what you do contact sentence show/hide
+        $('#you').hide();
+        $('#hi').click(function(){
+                $('#you').show('medium');
+        });
+        
+        
+        
+        // link hover animations for people on focus page
+        $("#steveDescription").hide();
+        $("#jeffDescription").hide();
+        $("#joelDescription").hide();
+        $("#daleDescription").hide();
+        $(".steve").hover(function(){
+			$("#steveDescription").show();
+		},function(){
+			$("#steveDescription").hide();
+	});
+        
+        $(".jeff").hover(function(){
+			$("#jeffDescription").show();
+		},function(){
+			$("#jeffDescription").hide();
+	});
+        
+        $(".joel").hover(function(){
+			$("#joelDescription").show();
+		},function(){
+			$("#joelDescription").hide();
+	});
+        
+        $(".dale").hover(function(){
+			$("#daleDescription").show();
+		},function(){
+			$("#daleDescription").hide();
+	});
+        
+        
 });
